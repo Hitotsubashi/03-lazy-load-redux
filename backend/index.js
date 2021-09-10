39af705cba28215348c0217e287af077c468714d
Hitotsubashi <1220043823@qq.com>
2021-09-10 23:46:12 +0800

    完成后端配置

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+
+const USERS = [
+  {
+    id:'0',
+    name:'用户A',
+    group_id:'0'
+  }
+]
+
+const GROUPS=[
+  {
+    id:'0',
+    name:'分组A'
+  }
+]
+
+const PORT=8888
+
+app.get('/users', function (req, res) {
+  res.send({users:USERS})
+})
+
+app.get('/groups', function (req, res) {
+  setTimeout(() => {
+    res.send({groups:GROUPS})
+  }, 1000);
+})
+
+app.listen(PORT)
