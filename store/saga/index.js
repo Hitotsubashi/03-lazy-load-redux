1695ee4277ed0bb7c79c50f66465cc6491cf6a34
Hitotsubashi <1220043823@qq.com>
2021-09-11 17:35:11 +0800

    save

--- store/saga/index.js
+++ store/saga/index.js
@@ -2,8 +2,19 @@ import { eventChannel, buffers } from 'redux-saga';
 
 let trigger = null
 
-
+const handler = {
+  get: (target, property) => {
+    if(!(typeof property==='string'&&/\d+/.test(property))) return target[property]
+    if (!(property in target)) {
+      if (trigger instanceof Function) {
+        trigger({});
+      }
+      return '加载中';
+    }
+    return target[property];
+  },
+};
 
 const makeGroupProxy = (target)=>{
-
+  return new Proxy(target, handler);
 }
